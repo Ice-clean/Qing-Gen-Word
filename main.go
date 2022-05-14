f9533ae619ac1b4cdf1c4cacd9fe4e8fde2564ef
IceClean <1419263382@qq.com>
2022-05-14 09:20:48 +0800

    feat: 成功生成结构体

--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"QingWork/model"
-	_ "QingWork/model"
 	"fmt"
+	"gorm.io/gen"
 )
 
 func main() {
 	err := model.DB.AutoMigrate(&model.User{})
 	if err != nil {
 		fmt.Println("自动迁移 User 表失败！")
+		return
 	}
+	fmt.Println("自动迁移 User 表成功！")
+
+	// 自动生成 struct
+	GeneratePeopleStruct()
+}
+
+// GeneratePeopleStruct 生成 People 结构体
+func GeneratePeopleStruct() {
+	g := gen.NewGenerator(gen.Config{
+		OutFile: "/tmp/QingWord/query",
+	})
+
+	g.UseDB(model.DB)
+	g.ApplyBasic(g.GenerateAllTable()...)
+	g.Execute()
 }
